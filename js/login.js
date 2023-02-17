2fe017081d8d4173ba27f78380283d246b5c983d
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-02-17 15:16:19 +0600

    some fixed

--- js/login.js
+++ js/login.js
@@ -4,18 +4,19 @@ document.getElementById('btn-submit').addEventListener("click", function () {
     // always remember to use .value to get text from an input field
     const emailField = document.getElementById('user-email');
     const email = emailField.value;
-    emailField.value="";
+    emailField.value = "";
     // step-3 get the password
     // 1.set the id on the html element
     // 2.get the element
     // 3.get the value from, the element
     const passwordField = document.getElementById('user-password');
     const password = passwordField.value;
-    passwordField.value="";
+    passwordField.value = "";
 
     // DANGER:DO NOT VERIFY EMAIL OR PASSWORD ON THE CLIENT SIDE
     // step:4 verify emaail and password
     if (email === 'shontan@baap.com' && password === 'taka de') {
+        window.location.href = "bank.html";
         console.log("Valid User");
     }
     else {
